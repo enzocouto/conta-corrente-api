3a00a4bcbe50b74dff04eb683fe891226e1f40ec
Couto <enzo.couto@capgemini.com>
2022-06-14 12:06:50 -0300

    add documentacao open-api - swagger

--- src/main/java/br/com/santanderbr/ib/contacorrente/api/ContaCorrenteApiApplication.java
+++ src/main/java/br/com/santanderbr/ib/contacorrente/api/ContaCorrenteApiApplication.java
@@ -3,9 +3,11 @@ package br.com.santanderbr.ib.contacorrente.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableWebMvc
 public class ContaCorrenteApiApplication {
 
 	public static void main(String[] args) {
